cfcdd2ddf6e5426be5ea2a0a8d36c6b0fd04c864
Елизавета Романова <soika-peresmeshnica2015@yandex.ru>
2024-10-15 21:02:37 +0300

    Главная страница

--- AvaloniaApplication1/ViewModels/AuthorizationViewModel.cs
+++ AvaloniaApplication1/ViewModels/AuthorizationViewModel.cs
@@ -1,10 +1,19 @@
 using System;
 using System.Collections.Generic;
 using ReactiveUI;
+using System.Linq;
+using AvaloniaApplication1.Models;
 
 namespace AvaloniaApplication1.ViewModels
 {
 	public class AuthorizationViewModel : ReactiveObject
 	{
-	}
+        AvaloniaTestBaseContext DBConnect;
+        List<Service> serviceList;
+        public AuthorizationViewModel(AvaloniaTestBaseContext dBConnect)
+        {
+            DBConnect = dBConnect;
+            serviceList = DBConnect.Services.ToList();
+        }
+    }
 }
